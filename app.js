43166e8ab2e479222e5f5df0be222481ae14899d
maheen <maheenzuhra@gmail.com>
2026-02-20 13:06:30 +0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function borderCHange(e){
-    e.style.border ="1px solid red"
+    e.style.border ="1px solid red";
+    e.style.width = "300px"
 
 }
 function borderRemove(e){
-    e.style.border ="2px solid green"
+    e.style.border ="2px solid green";
+    e.style.width = "200px"
 }
